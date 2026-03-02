6c2fc2782aef3408d0c61b230555151cb87c1c01
Piotr Szweda <szweda@cyberstudio.pl>
2026-03-03 00:15:45 +0100

    fix(03-05): add Tailwind CSS to Storybook build (postcss config + storybook.css)

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,7 @@
 import type { Preview } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import "../tokens.css";
+import "../src/storybook.css";
 
 const preview: Preview = {
   decorators: [
